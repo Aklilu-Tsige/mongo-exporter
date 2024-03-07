bc099503b7d0714d1271dd769c482cdad9033fd6
Aklilu Tsige <aklilu.tsige.w@gmail.com>
2024-03-07 19:35:03 +0100

    demo init spring zeebe client

---
+++ mongodb-exporter-service/src/test/java/com/demo/mongodbexporter/MongodbExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mongodbexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
